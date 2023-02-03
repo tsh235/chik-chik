a8639cfade91f812aac7c2dad3e69c212148ffce
tsh235 <tanyashustrova@yandex.ru>
2023-02-03 13:10:37 +0300

    final

--- js/script.js
+++ js/script.js
@@ -130,36 +130,6 @@ const renderService = (wrapper, data) => {
   wrapper.append(...labels);
 };
 
-const renderSpec = (wrapper, data) => {
-  const labels = data.map(item => {
-    const label = document.createElement('label');
-    label.classList.add('radio');
-    label.innerHTML = `
-      <input class="radio__input" type="radio" name="spec" value="${item.id}">
-      <span class="radio__label radio__label--spec" style="--bg-img: url(${API_URL}${item.img})">${item.name}</span>
-    `;
-    return label;
-  });
-
-  wrapper.append(...labels);
-};
-
-const renderMonth = (wrapper, data) => {
-  const labels = data.map(item => {
-    console.log('labels: ', labels);
-
-    const label = document.createElement('label');
-    label.classList.add('radio');
-    label.innerHTML = `
-      <input class="radio__input" type="radio" name="month" value="${item}">
-      <span class="radio__label">${new Intl.DateTimeFormat('ru-RU', {month: 'long'}).format(new Date(item))}</span>
-    `;
-    return label;
-  });
-
-  wrapper.append(...labels);
-};
-
 const initService = () => {
   const priceList = document.querySelector('.price__list');
   const reserveFieldsetService = document.querySelector('.reserve__fieldset--service');
@@ -192,11 +162,67 @@ const removeDisabled = (arr) => {
   arr.forEach(elem => {
     elem.disabled = false;
   })
-}
+};
+
+const renderSpec = (wrapper, data) => {
+  const labels = data.map(item => {
+    const label = document.createElement('label');
+    label.classList.add('radio');
+    label.innerHTML = `
+      <input class="radio__input" type="radio" name="spec" value="${item.id}">
+      <span class="radio__label radio__label--spec" style="--bg-img: url(${API_URL}${item.img})">${item.name}</span>
+    `;
+    return label;
+  });
+
+  wrapper.append(...labels);
+};
+
+const renderMonth = (wrapper, data) => {
+  const labels = data.map(month => {
+    const label = document.createElement('label');
+    label.classList.add('radio');
+    label.innerHTML = `
+      <input class="radio__input" type="radio" name="month" value="${month}">
+      <span class="radio__label">${new Intl.DateTimeFormat('ru-RU', {month: 'long'}).format(new Date(2023, month, 0))}</span>
+    `;
+    return label;
+  });
+
+  wrapper.append(...labels);
+};
+
+const renderDay = (wrapper, data, month) => {
+  const labels = data.map(day => {
+    const label = document.createElement('label');
+    label.classList.add('radio');
+    label.innerHTML = `
+      <input class="radio__input" type="radio" name="day" value="${day}">
+      <span class="radio__label">${new Intl.DateTimeFormat('ru-RU', {month: 'long', day: 'numeric'}).format(new Date(`2023, ${month}/${day}`))}</span>
+    `;
+    return label;
+  });
+
+  wrapper.append(...labels);
+};
+
+const renderTime = (wrapper, data) => {
+  const labels = data.map(time => {
+    const label = document.createElement('label');
+    label.classList.add('radio');
+    label.innerHTML = `
+      <input class="radio__input" type="radio" name="time" value="${time}">
+      <span class="radio__label">${time}</span>
+    `;
+    return label;
+  });
+
+  wrapper.append(...labels);
+};
 
 const initReserve = () => {
   const reserveForm = document.querySelector('.reserve__form');
-  const { fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn } = reserveForm;
+  const { fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn } = reserveForm;
 
   addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
 
@@ -214,22 +240,79 @@ const initReserve = () => {
       renderSpec(fieldspec, data);
       removePreload(fieldspec);
       removeDisabled([fieldspec]);
-    }
+    };
 
     if (target.name === 'spec') {
       addDisabled([fielddata, fieldmonth, fieldday, fieldtime, btn]);
       addPreload(fieldmonth);
 
-      const response = await fetch(`${API_URL}/api?spec=${target.value}`);
+      const response = await fetch(`${API_URL}/api?spec=${reserveForm.spec.value}`);
       const data = await response.json();
 
       fieldmonth.textContent = ' ';
       renderMonth(fieldmonth, data);
       removePreload(fieldmonth);
       removeDisabled([fielddata, fieldmonth]);
-      
-    }
+    };
+
+    if (target.name === 'month') {
+      addDisabled([fieldday, fieldtime, btn]);
+      addPreload(fieldday);
+
+      const response = await fetch(`${API_URL}/api?spec=${reserveForm.spec.value}&month=${reserveForm.month.value}`);
+      const data = await response.json();
+
+      fieldday.textContent = ' ';
+      renderDay(fieldday, data, reserveForm.month.value);
+      removePreload(fieldday);
+      removeDisabled([fieldday]);
+    };
+
+    if (target.name === 'day') {
+      addDisabled([fieldtime, btn]);
+      addPreload(fieldtime);
+
+      const response = await fetch(`${API_URL}/api?spec=${reserveForm.spec.value}&month=${reserveForm.month.value}&day=${target.value}`);
+      const data = await response.json();
+
+      fieldtime.textContent = ' ';
+      renderTime(fieldtime, data);
+      removePreload(fieldtime);
+      removeDisabled([fieldtime]);
+    };
+
+    if (target.name === 'time') {
+      removeDisabled([btn]);
+    };
   });
+
+  reserveForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(reserveForm);
+    const json = JSON.stringify(Object.fromEntries(formData));
+
+    const response = await fetch(`${API_URL}api/order`, {
+      method: 'post',
+      body: json,
+    });
+
+    const data = await response.json();
+
+    addDisabled([fieldservice, fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
+
+    const p = document.createElement('p');
+    // p.classList.add('') это добавляем для стилизации
+    p.textContent = `
+      Спасибо за бронь №${data.id}!
+      Ждем вас ${new Intl.DateTimeFormat('ru-RU', {
+        month: 'long',
+        day: 'numeric',
+      }).format(new Date(`2023, ${data.month}/${data.day}`))}, 
+      время ${data.time}
+    `;
+    reserveForm.append(p);
+  })
 }
 
 const init = () => {
